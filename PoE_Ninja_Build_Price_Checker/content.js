ab37050fee3781af84224ad4abd294a12c13b6e1
Jason Hasselle <41556060+Jhasselle@users.noreply.github.com>
2019-01-10 17:22:30 -0800

    Add files via upload

--- PoE_Ninja_Build_Price_Checker/content.js
+++ PoE_Ninja_Build_Price_Checker/content.js
@@ -1,88 +1,79 @@
-var ninjaAccessory = new XMLHttpRequest();
-var ninjaArmour = new XMLHttpRequest();
-var ninjaFlask = new XMLHttpRequest();
-var ninjaJewel = new XMLHttpRequest();
-var ninjaWeapon = new XMLHttpRequest();
+// Welcome to the Spaghetti Factory...
 
-var poeCharacterInventory= new XMLHttpRequest();
-var poeCharacterJewels = new XMLHttpRequest();
+chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
+	console.log('Attemping to get URL');
+    var currentURL = tabs[0].url;
+    console.log('Found: ', currentURL);
+    start(currentURL);
+});
 
-var currentURL = window.location.href;
+function start(currentURL) {
+	var ninjaCharacter = new XMLHttpRequest();
+	var ninjaAccessory = new XMLHttpRequest();
+	var ninjaArmour = new XMLHttpRequest();
+	var ninjaFlask = new XMLHttpRequest();
+	var ninjaJewel = new XMLHttpRequest();
+	var ninjaWeapon = new XMLHttpRequest();
 
-console.log(currentURL);
-var names = getNames(currentURL);
-var accountName = names[0];
-console.log('Account name:', accountName);
-var characterName = names[1];
-console.log('Character name:', characterName);
+	var names = getNames(currentURL);
+	var accountName = names[0];
+	var characterName = names[1];
 
-// poe.ninja Prices JSON
-ninjaAccessory.open('GET', 'https://poe.ninja/api/Data/GetUniqueAccessoryOverview?league=Betrayal');
-ninjaAccessory.responseType = 'json';
-ninjaAccessory.send();
+	$(function(){$('#account').text(accountName);});
+	$(function(){$('#character').text(characterName);});
 
-ninjaArmour.open('GET', 'https://poe.ninja/api/Data/GetUniqueArmourOverview?league=Betrayal');
-ninjaArmour.responseType = 'json';
-ninjaArmour.send();
+	// poe.ninja Prices JSON
+	ninjaAccessory.open('GET', 'https://poe.ninja/api/Data/GetUniqueAccessoryOverview?league=Betrayal');
+	ninjaAccessory.responseType = 'json';
+	ninjaAccessory.send();
 
-ninjaFlask.open('GET', 'https://poe.ninja/api/Data/GetUniqueFlaskOverview?league=Betrayal');
-ninjaFlask.responseType = 'json';
-ninjaFlask.send();
+	ninjaArmour.open('GET', 'https://poe.ninja/api/Data/GetUniqueArmourOverview?league=Betrayal');
+	ninjaArmour.responseType = 'json';
+	ninjaArmour.send();
 
-ninjaJewel.open('GET', 'https://poe.ninja/api/Data/GetUniqueJewelOverview?league=Betrayal');
-ninjaJewel.responseType = 'json';
-ninjaJewel.send();
+	ninjaFlask.open('GET', 'https://poe.ninja/api/Data/GetUniqueFlaskOverview?league=Betrayal');
+	ninjaFlask.responseType = 'json';
+	ninjaFlask.send();
 
-ninjaWeapon.open('GET', 'https://poe.ninja/api/Data/GetUniqueWeaponOverview?league=Betrayal');
-ninjaWeapon.responseType = 'json';
-ninjaWeapon.send();
+	ninjaJewel.open('GET', 'https://poe.ninja/api/Data/GetUniqueJewelOverview?league=Betrayal');
+	ninjaJewel.responseType = 'json';
+	ninjaJewel.send();
 
-// Character Items JSON
+	ninjaWeapon.open('GET', 'https://poe.ninja/api/Data/GetUniqueWeaponOverview?league=Betrayal');
+	ninjaWeapon.responseType = 'json';
+	ninjaWeapon.send();
 
-var inventorySearchString = 'https://www.pathofexile.com/character-window/get-items?accountName=' + accountName + '&character=' + characterName;
-poeCharacterInventory.open('GET', inventorySearchString);
-poeCharacterInventory.responseType = 'json';
-poeCharacterInventory.send();
-
-var jewelSearchString = 'https://www.pathofexile.com/character-window/get-passive-skills?accountName=' + accountName + '&character=' + characterName + '&reqData=0';
-poeCharacterJewels.open('GET', jewelSearchString);
-poeCharacterJewels.responseType = 'json';
-poeCharacterJewels.send();
+	ninjaCharacter.open('GET', 'https://poe.ninja/api/data/458db603f91b064de6bc85dc381caa2d/getcharacter?overview=betrayal&account=' + accountName + '&name=' + characterName);
+	ninjaCharacter.responseType = 'json';
+	ninjaCharacter.send();
 
 
+	ninjaCharacter.onreadystatechange = function() {
+	    if (this.readyState == 4 && this.status == 200) {
+	       console.log('poe ninja char:', this.response);
+	    }
+	}
 
-ninjaArmour.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-        console.log('poe.ninja armour: ', this.response);
-    }
+
+	setTimeout(function() {
+	    if ((ninjaAccessory.readyState == 4 && ninjaAccessory.status == 200) 
+	        && (ninjaCharacter.readyState == 4 && ninjaCharacter.status == 200)) {
+	        	console.log('We are ready!');
+	        //Most definitely an acceptable length
+	        var cost = calculateCharacterCost(ninjaAccessory.response, ninjaArmour.response, ninjaFlask.response, ninjaWeapon.response, ninjaCharacter.response);
+	        cost += calculateJewelCost(ninjaJewel.response, ninjaCharacter.response);
+	        costString = cost + ' chaos';
+
+	        $(function(){$('#totalCost').text(costString);});
+	        console.log('Estimated cost: ', cost, 'chaos');
+	    }
+	    else {
+	        console.log('We are not ready!');
+	    }
+	}, 2000);
+
+
 }
-
-poeCharacterInventory.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-        console.log('Character inventory items: ', this.response);
-    }
-}
-
-poeCharacterJewels.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-        console.log('Character tree jewels: ', this.response);
-    }
-}
-
-setTimeout(function() {
-    if ((ninjaWeapon.readyState == 4 && ninjaWeapon.status == 200) 
-        && (poeCharacterInventory.readyState == 4 && poeCharacterInventory.status == 200)) {
-        console.log('We are ready!');
-
-     	// ...Totally not a terrible length...
-        var cost = calculateCharacterCost(ninjaAccessory.response, ninjaArmour.response, ninjaFlask.response, ninjaWeapon.response, poeCharacterInventory.response);
-        cost += calculateJewelCost(ninjaJewel.response, poeCharacterJewels.response);
-        console.log('Estimated cost: ', cost, 'chaos');
-    }
-    else {
-        console.log('We are not ready!');
-    }
-}, 2000);
 
 
 function getNames(url) {
@@ -114,23 +105,29 @@ function getNames(url) {
 function calculateJewelCost(jewelPrices, jewels) {
 
 	var totalChaos = 0;
-	var numOfJewels = Object.keys(jewels['items']).length;
+	var itemValue = 0;
+	var numOfJewels = Object.keys(jewels['jewels']).length;
 	var jewelPricesLength = Object.keys(jewelPrices['lines']).length;
-	// console.log('JEWELS!!!!!');
-	// console.log(jewels);
-	// console.log(jewelPrices);
-	// console.log(jewelPricesLength);
+	var jewelIndex = 0;
+	var itemName;
+	var itemClass;
+	var itemImgUrl;
 
 	for (var i = 0; i < numOfJewels; i++){
 		console.log('___________________________________');
-		var itemName = jewels['items'][i]['name'];
+		itemName = jewels['jewels'][i]['itemData']['name'];
+		itemSlot = jewels['jewels'][i]['itemSlot'];
+		itemClass = jewels['jewels'][i]['itemClass'];
+		itemImgUrl = jewels['jewels'][i]['itemData']['icon'];
 
-		if (jewels['items'][i]['frameType'] == 3) {
+		// 3 means it is of 'unique item' type
+		if (itemClass == 3) {
 			for (var j = 0; j < jewelPricesLength; j++) {
 				if (itemName == jewelPrices['lines'][j]['name']){
 					console.log(itemName, 'found.');
 					console.log('poe.ninja prices: ', jewelPrices['lines'][j]['chaosValue']);
-					totalChaos += jewelPrices['lines'][j]['chaosValue'];
+					itemValue = jewelPrices['lines'][j]['chaosValue'];
+					totalChaos += itemValue;
 				}
 			}
 		}
@@ -138,6 +135,9 @@ function calculateJewelCost(jewelPrices, jewels) {
 			console.log(itemName, 'is a rare jewel.');
 			console.log('A later version will support price estimation for rare jewels.');
 		}
+
+		jewelIndex = i + 1;
+		updateHTML(itemSlot, itemName, itemClass, itemImgUrl, itemValue, jewelIndex);
 	}
 
 	return totalChaos;
@@ -147,57 +147,91 @@ function calculateCharacterCost(accessoryPrice, armourPrice, flaskPrice, weaponP
     
     var totalChaos = 0;
     var numOfInventoryItems = Object.keys(inventory['items']).length;
-    console.log('This character has', numOfInventoryItems, 'inventory items.');
+    var numOfFlasks = Object.keys(inventory['flasks']).length;
+    var item;
+    var itemName;
+    var itemType;
+    var itemSlot;
+    var itemClass;
+    var itemImgUrl;
+	var itemValue;
+	var flaskIndex = 0;
 
     for (var i = 0; i < numOfInventoryItems; i++) {
-		var item = Object.entries(inventory['items'])[i];
-        var itemName = inventory['items'][i]['name'];
-        var itemType = Object.keys(inventory['items'][i]['category'])[0];
+		//var item = Object.entries(inventory['items'])[i];
+        item = inventory['items'][i]['itemData'];
+        itemName = inventory['items'][i]['itemData']['name'];
+        itemType = Object.keys(inventory['items'][i]['itemData']['category'])[0];
+        itemSlot = inventory['items'][i]['itemSlot'];
+        itemClass = inventory['items'][i]['itemClass'];
+        itemImgUrl = inventory['items'][i]['itemData']['icon'];
+
+        console.log(item, itemName, itemType);
 
         // This condition will not be necessary once prices of rare items 
         // can be estimated. Until then, we shall skip them.
-        if (itemName == ''){
+        if (itemName == undefined){
             console.log('Rare Item', itemType)
         }
         else {
             //console.log(item, itemType, item[1]['name'])
+            itemValue = 0;
+
+            var success = true;
 
             if (itemType == 'accessories'){
-                totalChaos += findInventoryItemCost(accessoryPrice, item, itemType);
+                itemValue = findInventoryItemCost(accessoryPrice, item, itemType);
             }
             else if (itemType == 'armour'){
-                totalChaos += findInventoryItemCost(armourPrice, item, itemType);
-            }
-            else if (itemType == 'flasks'){
-                totalChaos += findInventoryItemCost(flaskPrice, item, itemType);
+                itemValue = findInventoryItemCost(armourPrice, item, itemType);
             }
             else if (itemType == 'weapons'){
-                totalChaos += findInventoryItemCost(weaponPrice, item, itemType);
+                itemValue = findInventoryItemCost(weaponPrice, item, itemType);
             }
             else {
                 console.log('Unknown item type. FeelsBadMan');
+                success = false;
+            }
+
+            if (success) {
+            	totalChaos += itemValue;
+            	updateHTML(itemSlot, itemName, itemClass, itemImgUrl, itemValue, flaskIndex);
             }
 
         }
     }
+
+    for (var i = 0; i < numOfFlasks; i++){
+    	itemValue = 0;
+    	item = inventory['flasks'][i]['itemData'];
+        itemType = Object.keys(inventory['flasks'][i]['itemData']['category'])[0];
+        itemSlot = inventory['flasks'][i]['itemSlot'];
+        itemImgUrl = inventory['flasks'][i]['itemData']['icon'];
+        itemClass = inventory['flasks'][i]['itemClass'];
+
+        if (itemClass == 3){
+        	itemName = inventory['flasks'][i]['itemData']['name'];
+        }
+        else{
+        	itemName = inventory['flasks'][i]['itemData']['typeLine'];
+        }
+
+		itemValue = findInventoryItemCost(flaskPrice, item, itemType);
+		flaskIndex = i + 1;
+		updateHTML(itemSlot, itemName, itemClass, itemImgUrl, itemValue, flaskIndex);
+		totalChaos += itemValue;
+               
+    }
     return totalChaos;
 }
-
-/**
-*   Finds the cost (in chaos orbs) of a given item and a price list JSON from poe.ninja
-*
-*   @param {The JSON storing prices} itemList
-*   @param {The JSON of the item we're price-checking} item
-*	@param {String representing the item type} itemType
-*/
 
 function findInventoryItemCost(itemList, item, itemType) {
 
 	console.log('___________________________________');
-	console.log(item[1]['name']);
+	console.log(item['name']);
 
 	// For itemList
-	var chaosValue = 1;
+	var chaosValue = 0;
     var numOfItems = Object.keys(itemList['lines']).length;
 
     // For item
@@ -207,27 +241,18 @@ function findInventoryItemCost(itemList, item, itemType) {
 	var numOfAbyss = 0;
 
 	try {
-		numOfSockets = Object.keys(item[1]['sockets']).length;
-		// console.log(item[1]);
-		// console.log(item[1]['name']);
-		// console.log('Num of sockets =', numOfSockets);
-		
-		// For checking a backwards 5 link
-		// Contiguous links share the same group.
-		// If the 2nd socket is of group 2, then it has the potential 
-		// to be the start of a 5-link group
-		if (numOfSockets >= 5 && item[1]['sockets'][1]['group'] == 1){
+		numOfSockets = Object.keys(item['sockets']).length;
+		console.log('num of sockets:', numOfSockets);
+		if (numOfSockets >= 5 && item['sockets'][1]['group'] == 1){
 			socketGroup = 1;
 		}
 
 		for (var i = 0; i < numOfSockets; i++) {
-			// console.log(item[1]['sockets'][i]['group']);
-
-			if (item[1]['sockets'][i]['group'] == socketGroup) {
+			if (item['sockets'][i]['group'] == socketGroup) {
 				numOfLinks += 1;
 			}
 
-			if (item[1]['sockets'][i]['attr'] == 'A') {
+			if (item['sockets'][i]['attr'] == 'A') {
 				numOfAbyss += 1;
 			}
 		}
@@ -240,7 +265,7 @@ function findInventoryItemCost(itemList, item, itemType) {
 	}
 	
     for (var i = 0; i < numOfItems; i++){
-        if (itemList['lines'][i]['name'] == item[1]['name']) {
+        if (itemList['lines'][i]['name'] == item['name']) {
 
         	console.log('Index = ', i);
 
@@ -252,7 +277,7 @@ function findInventoryItemCost(itemList, item, itemType) {
         	if (numOfAbyss == abyssSockets) {
         			chaosValue = itemList['lines'][i]['chaosValue'];
         			break;
-            		console.log('Chaos value of', item[1]['name'], ' = ', chaosValue);
+            		console.log('Chaos value of', item['name'], ' = ', chaosValue);
         		}
 
         	else if ((itemType == 'armour' || itemType == 'weapons') && numOfLinks >= 5) {
@@ -260,13 +285,13 @@ function findInventoryItemCost(itemList, item, itemType) {
         		if (itemList['lines'][i]['links'] == numOfLinks) {
         			chaosValue = itemList['lines'][i]['chaosValue'];
         			break;
-            		console.log('Chaos value of', item[1]['name'], ' = ', chaosValue);
+            		console.log('Chaos value of', item['name'], ' = ', chaosValue);
         		}
         	}
         	else {
         		console.log(i, itemList['lines'][i]['id'], itemList['lines'][i]['chaosValue']);
             	chaosValue = itemList['lines'][i]['chaosValue'];
-            	console.log('Chaos value of', item[1]['name'], ' = ', chaosValue);
+            	console.log('Chaos value of', item['name'], ' = ', chaosValue);
 
         	}
         }
@@ -275,6 +300,82 @@ function findInventoryItemCost(itemList, item, itemType) {
     console.log('Chaos predicted: ', chaosValue);
     console.log('___________________________________');
     return chaosValue;
+}
+
+function updateHTML(itemSlot, itemName, itemClass, url, value, index) {
+
+
+	var id = '';
+	var imgId = '';
+
+	switch(itemSlot) {
+		case 1:
+			id = '#helmet';
+			break;
+		case 2:
+			id = '#gloves';
+			break;
+		case 3:
+			id = '#armour';
+			break;
+		case 4:
+			id = '#amulet';
+			break;
+		case 5:
+			id = '#boots';
+			break;
+		case 6:
+			id = '#offhand';
+			break;
+		case 7:
+			id = '#mainhand';
+			break;
+		case 8:
+			id = '#ring1';
+			break;
+		case 9:
+			id = '#ring2';
+			break;
+		case 10:
+			id = '#belt';
+			break;
+		case 11:
+		case 12:
+			id = '#jewel' + index;
+			break;
+		case 13:
+			id = '#flask' + index;
+			break;
+		default:
+	}
+
+	switch (itemClass){
+		case 0:
+			itemName += ' ( normal item )';
+			break;
+		case 1:
+			itemName += ' ( magic item )';
+			break;
+		case 2:
+			itemName += ' ( rare item )';
+			break;
+		case 3:
+			itemName += ' (' + value + ')';
+			break;
+		default:
+	}
+
+	imgId = id + 'Img';
+
+	console.log('************************');
+	console.log(itemSlot, itemName, url);
+	console.log(id, imgId);
+	console.log('************************');
+
+
+	$(function(){$(id).text(itemName);});
+	$(function(){$(imgId).attr('src', url);});
+
 }
 
 
